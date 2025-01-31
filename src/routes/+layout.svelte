7b1dfe0d628fc71f107a95d1c6bcc2bfd914d110
Pattarathon Watanakij <luck.wtj@gmail.com>
2025-02-01 02:04:39 +0700

    UI: Add space layout for the menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,21 +8,44 @@
 </svelte:head>
 
 <!-- Layout for normal PC / Ipad -->
-<div class="hidden lg:flex w-screen h-screen flex flex-row">
-	<div class="w-1/4 h-full bg-[#2A2A2A]">
+<div class="flex hidden h-screen w-screen flex-row lg:flex">
+	<div class="flex h-full w-1/4 flex-col items-center justify-center gap-5 bg-[#2A2A2A] px-5 py-5">
+		<div class="flex h-1/5 md:w-full xl:w-4/5 3xl:w-3/5 flex-col justify-center py-5">
+			<p class=" text-[#F4EFEF] text-3xl xl:text-5xl 3xl:text-6xl">Pattarathon W.</p>
+			<p class="text-2xl text-[#ADA5A5] xl:text-3xl 3xl:text-4xl">Software Engineer</p>
+		</div>
+		<div class="h-2/5 md:w-full  xl:w-4/5 3xl:w-3/5 flex flex-col">
+			<p class="w-full h-1/5 2xl:text-4xl font-bold text-[#ADA5A5]">Explore</p>
+			<div class="w-full h-1/5 flex flex-row items-center gap-5">
+				<div class="w-1/5 h-4/5 bg-pink-500"></div>
+				<p class="w-4/5 h-4/5 flex items-center  2xl:text-4xl text-[#F4EFEF] font-bold">Home</p>
+			</div>
+			<div class="w-full h-1/5 flex flex-row items-center gap-5 ">
+				<div class="w-1/5 h-4/5 bg-pink-500"></div>
+				<p class="w-4/5 h-4/5 flex items-center  2xl:text-4xl text-[#F4EFEF] font-bold">Experience</p>
+			</div>
+			<div class="w-full h-1/5 flex flex-row items-center gap-5 ">
+				<div class="w-1/5 h-4/5 bg-pink-500"></div>
+				<p class="w-4/5 h-4/5 flex items-center  2xl:text-4xl text-[#F4EFEF] font-bold">Projects</p>
+			</div>
+			<div class="w-full h-1/5 flex flex-row items-center gap-5">
+				<div class="w-1/5 h-4/5 bg-pink-500"></div>
+				<p class="w-4/5 h-4/5 flex items-center  2xl:text-4xl text-[#F4EFEF] font-bold">About</p>
+			</div>
+			
+		</div>
+		<div class="h-2/5 md:w-full xl:w-4/5 3xl:w-3/5 bg-green-500"></div>
 	</div>
-	<div class="w-3/4 h-full bg-[#212121] py-5 px-5">
+	<div class="h-full w-3/4 bg-[#212121] px-5 py-5">
 		{@render children()}
 	</div>
 </div>
 
 <!-- Layout for mobile -->
-<div class="flex lg:hidden xl:hidden 2xl:hidden 3xl:hidden w-screen h-screen flex-col">
-	<div class="w-full h-4/5 bg-[#212121] py-5 px-5">
+<div class="flex h-screen w-screen flex-col lg:hidden xl:hidden 2xl:hidden 3xl:hidden">
+	<div class="h-4/5 w-full bg-[#212121] px-5 py-5">
 		{@render children()}
 	</div>
 	<!-- Menu -->
-	 <div class="w-full h-1/5 bg-[#2A2A2A]">
-
-	 </div>
+	<div class="h-1/5 w-full bg-[#2A2A2A]"></div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <div class="w-full h-full flex justify-center items-center">
     <div class="w-4/5 h-full flex flex-col justify-center items-center gap-5 3xl:gap-10">
         <h1 class="w-full text-6xl 3xl:text-9xl text-[#F4EFEF]">👋 Hello, I’m Pattarathon Watanakij A Software Engineer</h1>
-        <p class="w-full text-4xl 3xl:text-5xl text-[#F4EFEF]">I’m a versatile software developer with expertise in website, blockchain and mobile application development.</p>
-        
+        <p class="w-full text-4xl 3xl:text-5xl text-[#ADA5A5]">I’m a versatile software developer with expertise in website, blockchain and mobile application development.</p>
     </div>
 </div>
