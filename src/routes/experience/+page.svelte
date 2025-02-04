a58e609e2bde44c2e8d09f02198e0e241aa398e6
Pattarathon Watanakij <luck.wtj@gmail.com>
2025-02-05 01:06:30 +0700

    Add layout for experience

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -1,15 +1,62 @@
-<div class="w-full h-full flex flex-col gap-5 px-5 py-5">
-    <div class="w-[90%] h-1/4 flex flex-col py-5 gap-10">
-        <p class="w-full text-6xl font-bold text-[#F4EFEF]">Experience</p>
-        <p class="w-full h-2/4 text-3xl font-thin text-[#ADA5A5]">
-            Over the past few years, I’ve worked extensively in the crypto industry, collaborating with web3 projects while also contributing to web development agencies on scalable platforms
-        </p>
-    </div>
-    <div class="w-full h-3/4 flex flex-col gap-5">
-        <div class="w-[90%] h-1/5 bg-red-500">
+<div class="flex h-full w-full flex-col gap-5 px-5 py-5">
+	<div class="flex h-1/4 w-[90%] flex-col gap-10 py-5">
+		<p class="w-full text-6xl font-bold text-[#F4EFEF]">Experience</p>
+		<p class="h-2/4 w-full text-3xl font-thin text-[#ADA5A5]">
+			Over the past few years, I’ve worked extensively in the crypto industry, collaborating with
+			web3 projects while also contributing to web development agencies on scalable platforms
+		</p>
+	</div>
+	<div class="flex h-3/4 w-full flex-col gap-5">
+		<div class="w-[90%] h-[10%] flex flex-row items-center justify-between ">
+            <div class="w-[5%] flex justify-center items-center h-full rounded-[50%] font-bold text-[#F4EFEF] bg-[#292929]">1</div>
+            <div class="w-[40%] h-[10%] bg-[#ADA5A5] "></div>
+            <div class="w-[5%] flex justify-center items-center h-full rounded-[50%] font-bold text-[#F4EFEF] bg-[#292929]">2</div>
+            <div class="w-[40%] h-[10%] bg-[#ADA5A5] "></div>
+            <div class="w-[5%] flex justify-center items-center h-full rounded-[50%] font-bold text-[#F4EFEF] bg-[#292929]">3</div>
         </div>
-        <div class="w-[90%] h-4/5 rounded-xl bg-purple-500">
-
-        </div>
-    </div>
+		<div class="flex h-4/5 w-[90%] flex-col rounded-3xl bg-[#292929] px-5 py-10">
+			<div class="flex h-1/5 w-full flex-row justify-between">
+				<div class="h-full w-2/5 text-5xl font-bold text-[#ADA5A5]">2023 - Present</div>
+				<div class="h-full w-3/5 text-end text-5xl font-bold text-[#F4EFEF]">
+					Project Owner · BonkDAO
+				</div>
+			</div>
+			<div class="flex h-3/5 w-full flex-col">
+				<p class="h-full w-full text-3xl text-[#D3D3D3]">
+					I independently designed, developed, and launched a blockchain platform from the ground
+					up, deploying on multiple networks, including Binance Smart Chain and TON. I handled
+					everything—from smart contract development and full-stack implementation to marketing and
+					community building. Through strategic growth initiatives, I scaled the project from zero
+					users to 570 active participants and fostered a thriving community of over 2,500 members
+				</p>
+			</div>
+			<div class="flex h-[10%] w-full flex-row justify-between gap-5">
+				<div
+					class="flex h-full w-1/5 items-center justify-center rounded-2xl bg-[#212121] text-2xl text-[#ADA5A5]"
+				>
+					Solidity
+				</div>
+				<div
+					class="flex h-full w-1/5 items-center justify-center rounded-2xl bg-[#212121] text-2xl text-[#ADA5A5]"
+				>
+					Ether.js
+				</div>
+				<div
+					class="flex h-full w-1/5 items-center justify-center rounded-2xl bg-[#212121] text-2xl text-[#ADA5A5]"
+				>
+					FunC
+				</div>
+				<div
+					class="flex h-full w-1/5 items-center justify-center rounded-2xl bg-[#212121] text-2xl text-[#ADA5A5]"
+				>
+					React
+				</div>
+				<div
+					class="flex h-full w-1/5 items-center justify-center rounded-2xl bg-[#212121] text-2xl text-[#ADA5A5]"
+				>
+					GoLang
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -8,7 +8,56 @@
 		</p>
 	</div>
 	<div class="flex h-3/4 w-full flex-col gap-5">
-		<div class="h-1/5 w-[90%] bg-red-500"></div>
-		<div class="h-4/5 w-[90%] rounded-xl bg-purple-500"></div>
+		<div class="w-[90%] h-[10%] flex flex-row items-center justify-between ">
+            <div class="w-[5%] flex justify-center items-center h-full rounded-[50%] font-bold text-[#F4EFEF] bg-[#292929]">1</div>
+            <div class="w-[40%] h-[10%] bg-[#ADA5A5] "></div>
+            <div class="w-[5%] flex justify-center items-center h-full rounded-[50%] font-bold text-[#F4EFEF] bg-[#292929]">2</div>
+            <div class="w-[40%] h-[10%] bg-[#ADA5A5] "></div>
+            <div class="w-[5%] flex justify-center items-center h-full rounded-[50%] font-bold text-[#F4EFEF] bg-[#292929]">3</div>
+        </div>
+		<div class="flex h-4/5 w-[90%] flex-col rounded-3xl bg-[#292929] px-5 py-10">
+			<div class="flex h-1/5 w-full flex-row justify-between">
+				<div class="h-full w-2/5 text-5xl font-bold text-[#ADA5A5]">2023 - Present</div>
+				<div class="h-full w-3/5 text-end text-5xl font-bold text-[#F4EFEF]">
+					Project Owner · BonkDAO
+				</div>
+			</div>
+			<div class="flex h-3/5 w-full flex-col">
+				<p class="h-full w-full text-3xl text-[#D3D3D3]">
+					I independently designed, developed, and launched a blockchain platform from the ground
+					up, deploying on multiple networks, including Binance Smart Chain and TON. I handled
+					everything—from smart contract development and full-stack implementation to marketing and
+					community building. Through strategic growth initiatives, I scaled the project from zero
+					users to 570 active participants and fostered a thriving community of over 2,500 members
+				</p>
+			</div>
+			<div class="flex h-[10%] w-full flex-row justify-between gap-5">
+				<div
+					class="flex h-full w-1/5 items-center justify-center rounded-2xl bg-[#212121] text-2xl text-[#ADA5A5]"
+				>
+					Solidity
+				</div>
+				<div
+					class="flex h-full w-1/5 items-center justify-center rounded-2xl bg-[#212121] text-2xl text-[#ADA5A5]"
+				>
+					Ether.js
+				</div>
+				<div
+					class="flex h-full w-1/5 items-center justify-center rounded-2xl bg-[#212121] text-2xl text-[#ADA5A5]"
+				>
+					FunC
+				</div>
+				<div
+					class="flex h-full w-1/5 items-center justify-center rounded-2xl bg-[#212121] text-2xl text-[#ADA5A5]"
+				>
+					React
+				</div>
+				<div
+					class="flex h-full w-1/5 items-center justify-center rounded-2xl bg-[#212121] text-2xl text-[#ADA5A5]"
+				>
+					GoLang
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
